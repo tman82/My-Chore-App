69cc8a833eb5e18b5d1e1c4eafc08c99bb1eaed2
Martrell Leonard <martrell_g@yahoo.com>
2020-11-27 12:41:06 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends React.Component {
   }
   
   componentDidMount() {
-    axios.get('/chores')
+    axios.get('http://localhost:4000/chores')
       .then(res => {
         const choresData = res.data;
         this.setState({ chores: choresData });
